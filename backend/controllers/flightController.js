1a6a575ad652e1b504e111ba8b02be9e8442fe24
Aqsa Mahmood <mahmoodaqsa77@gmail.com>
2023-11-24 18:29:33 +0500

    Update Flight Controller

--- backend/controllers/flightController.js
+++ backend/controllers/flightController.js
@@ -1,62 +1,83 @@
 const Flight = require("../models/flightModel");
 
-// Create Flight
-async function createFlight(req, res) {
-    console.log("Request Received");
-    try {
-        const flight = new Flight(req.body);
-        const savedflight = await flight.save();
-        console.log("Registered successfully");
-        res.status(201).send(savedflight);
-    } catch (err) {
-        console.error("Registration failed:", err);
-        res.status(500).send({ error: "Registration failed" });
-    }
+// Add Flight
+async function addFlight(req, res) {
+  console.log("Flight Add Request Received");
+  try {
+    const flight = new Flight(req.body); // Create a new flight instance from the request body
+    const savedFlight = await flight.save(); // Save the new flight to the database
+    console.log("Flight added successfully");
+    res.status(201).send(savedFlight); // Respond with the saved flight details
+  } catch (err) {
+    console.error("Flight addition failed:", err);
+    res.status(500).send({ error: "Flight addition failed" }); // Handle errors if the flight addition fails
+  }
 }
 
-// Update Flight 
+// Get Flight
+async function getFlight(req, res) {
+  try {
+    const flights = await Flight.find().exec();
+    if (!flights || flights.length === 0) {
+      return res.status(404).json({ message: "No flights found" });
+    }
+    res.status(200).json(flights);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+
+// Update Flight
 async function updateFlight(req, res) {
-    try {
-        const { id } = req.params;
-        const updateFlight = await Flight.findByIdAndUpdate(id, req.body, { new: true });
-        res.status(201).json(updateFlight);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const { id } = req.params;
+    var flight = {
+      // Define the properties to update based on the request body
+      Airline: req.body.Airline,
+      FlightNumber: req.body.FlightNumber,
+      DepartureAirport: req.body.DepartureAirport,
+      ArrivalAirport: req.body.ArrivalAirport,
+      Destination: req.body.Destination,
+      DepartureDate: req.body.DepartureDate,
+      ArrivalDate: req.body.ArrivalDate,
+      Class: req.body.Class,
+      AvailableSeats: req.body.AvailableSeats,
+      Price: req.body.Price,
+      // Add more properties as needed for flight details
+    };
+    const updateFlight = await Flight.findByIdAndUpdate(
+      id,
+      { $set: flight },
+      { new: true }
+    );
+    res.status(201).json(updateFlight);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
-// Search for flights
-async function searchFlights(req, res) {
-    try {
-        const { destination, departureDate, preferredClass } = req.query;
-
-        // Create a query object to dynamically construct the search query
-        const query = {};
-
-        // Add criteria to the query object based on available parameters
-        if (destination) {
-            query.Destination = destination;
-        }
-
-        if (departureDate) {
-            query.DepartureDate = new Date(departureDate); // Convert string to Date object
-        }
-
-        if (preferredClass) {
-            query.Class = preferredClass;
-        }
-
-        // Perform the search based on the constructed query
-        const flights = await Flight.find(query);
-
-        res.json(flights);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+// Deactivate Flight
+async function deactivateFlight(req, res) {
+  try {
+    const { id } = req.params;
+    const updatedFlight = await Flight.findByIdAndUpdate(
+      id,
+      { Active: false }, // Updating the 'Active' status of the flight to false
+      { new: true }
+    );
+    if (!updatedFlight) {
+      return res.status(404).json({ error: "Flight not found" });
     }
+    res.json(updatedFlight); // Send the updated flight data in the response
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
 module.exports = {
-  createFlight,
+  addFlight,
+  getFlight,
   updateFlight,
-  searchFlights,
+  deactivateFlight,
 };
